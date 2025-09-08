05144b496630bda936f3828d0a80bf167026966b
Kaito <kite670328@gmail.com>
2025-09-08 00:18:18 +0000

    delete. 不要なページを削除

--- frontend/app/linalg/mul/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client"
-import { useEffect } from 'react'
-import { useRouter } from 'next/navigation'
-
-export default function LegacyMulRedirect() {
-  const router = useRouter()
-  useEffect(() => { router.replace('/linalg/binary') }, [router])
-  return null
-}
